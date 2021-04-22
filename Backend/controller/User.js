2a24bc7073167cd3cd813b5af32367674b726a27
joaavet <jv397803@gmail.com>
2021-04-22 16:02:49 -0300

    primeiro

---
+++ Backend/controller/User.js
@@ -0,0 +1,39 @@
+const User = require('../model/User')
+
+//CRUD -Create - Read - Update - DELETE
+
+module.exports = {
+  async index(req,res){
+    let users = await User.find()
+    return res.json(users)
+  },
+
+  async store(req,res){
+    const {name, email, age, password} = req.body;
+    const user = await User.create({name, email, age, password})
+    return res.json(user)
+  },
+
+  async update(req,res){
+    //recupera o Id que pela url query e guarda na variavel id
+    var id = req.query.id;
+    //recupera as inf do post em 
+    
+    let user = await User.findById(id);
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.age = req.body.age;
+    user.password = req.body.password;
+    user = await User.updateOne({'_id': req.query.id} , user)
+
+    return res.json({messagem : 'Atualizar o user ' + id +' com os dados do post '+ user.name})
+  },
+   
+  async delete(req,res){
+    var id = req.query.id;
+    let user = await User.findById(id);
+    user = await User.deleteOne(user);
+    return res.json(user);
+    
+  }
+}
---
+++ Backend/src/index.js
@@ -0,0 +1,21 @@
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+
+const routes = require('./routes')
+
+const app = express()
+
+mongoose.connect('',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+
+app.use(express.json())   
+app.use(routes)
+
+app.listen(3000)
+
+console.log("servidor rodando no end: http://localhost:3000")
